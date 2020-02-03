4ee72a7e742b0fe8b1a14808a6283daa40821893
Dayana Matsakova <Dayana.Matsakova89@gmail.com>
2020-02-03 15:39:20 -0500

    Adding Xpath selectors to createDayReport

---
+++ test/specs/createDayReport.spec.js
@@ -0,0 +1,61 @@
+const { expect } = require('chai');
+
+const { URL_LOGIN, pageLoginSelectors, adminData, answers, pageDailyReportsSelectors, createDayReportPageSelectors } = require('./register_data');
+
+describe('DAY REPORT CREATE', () => {
+  before('Login as admin', () => {
+    browser.url(URL_LOGIN);
+    browser.$('//form//input[@name="email"]').setValue('admin@test.com');
+    browser.$('//form//input[@name="password"]').setValue('11111');
+    browser.$('//form//button[@type="submit"]').click();
+    browser.pause(1000);
+  });
+
+  it('should click Diary button', () => {
+    browser.$('//div[@id="site-menu"]//a[@qa="diary-link"]').click();
+    browser.pause(300);
+  });
+
+  it('should click Create day report button', () => {
+    browser.$('//a[@qa="create-day-report-button"]').click();
+    browser.pause(300);
+  });
+
+  it('should mark the checkbox', () => {
+    browser.$('//div[@qa="day-report-marks"]//input[@label="Helped classmates"]').click();
+  });
+
+  it('should choose one option in Morale dropdown', () => {
+    const dropdown = browser.$('//select[@name="morale"]');
+    dropdown.selectByVisibleText('9');
+  });
+
+  it('should fill out Hours field', () => {
+    const element = browser.$('//input[@name="hours"]');
+    element.setValue(5);
+  });
+
+  it('should fill out How was your day? field', () => {
+    const element = browser.$('//textarea[@name="description"]');
+    element.setValue('gogogogogogogoogogogogogogogogo');
+  });
+
+  it('should click Save button', () => {
+    const element = browser.$('//button[@type="submit"]');
+    element.click();
+    browser.pause(2000);
+  });
+
+  it('should first item in the list be equal created day report description', () => {
+    const actualText = browser.$('//div[@qa="day-report-item-0"]//div[@qa="description"]').getText();
+    const expectedText = 'gogogogogogogoogogogogogogogogo';
+    expect(actualText).eq(expectedText);
+  });
+
+  it('should first item in the list be equal created day report mark', () => {
+    const actualText = browser.$('//div[@qa="day-report-item-0"]//div[@qa="labels"]//span').getText();
+    const expectedText = 'Helped classmates';
+    expect(actualText).eq(expectedText);
+  });
+
+});
--- test/specs/createFlashCard.spec.js
+++ test/specs/createFlashCard.spec.js
@@ -2,66 +2,77 @@ const { expect } = require('chai');
 
 const { URL_LOGIN, pageLoginSelectors, adminData, cardsPageData, flashCardsSelectors } = require('./register_data');
 
-describe('CREATE FLASH CARD', () => {
+describe('FLASH GROUP CREATE', () => {
   before('Login as admin', () => {
     browser.url(URL_LOGIN);
-    $(pageLoginSelectors.emailInput).setValue(adminData.login);
-    $(pageLoginSelectors.passwordInput).setValue(adminData.password);
-    $(pageLoginSelectors.loginButton).click();
-    browser.pause(2000);
-  });
-
-  it('should click Cards button', () => {
-    const element = $(flashCardsSelectors.cardsButton);
-    element.click();
+    browser.$('//form//input[@name="email"]').setValue('admin@test.com');
+    browser.$('//form//input[@name="password"]').setValue('11111');
+    browser.$('//form//button[@type="submit"]').click();
     browser.pause(1000);
   });
 
-  it('should check the header', () => {
-    const actual = $(flashCardsSelectors.h1).getText();
-    const expected = cardsPageData.h1;
-    expect(actual).equal(expected);
+  after('AFTER', () => {
+    browser.pause(3000);
   });
 
-  it('should click Create new FlashGroup button', () => {
-    const element = $(flashCardsSelectors.createNewFlashGroupButton);
-    element.click();
-    browser.pause(1000);
+  it('should click top menu Cards', () => {
+    browser.$('//div[@id="site-menu"]//a[@qa="cards-link"]').click();
   });
 
-  it('should check if modal body exists', () => {
-    const element = $(flashCardsSelectors.modalBody);
-    element.isExisting();
-    browser.pause(1000);
+  it('should click button Create new FlashGroup', () => {
+    browser.$('//button[@qa="flash-create-new-group"]').click();
+    browser.pause(500);
   });
 
-  it('should check the header Create Flash Group', () => {
-    const actual = $(flashCardsSelectors.h5Input).getText();
-    const expected = cardsPageData.h5;
-    expect(actual).equal(expected);
+  it('should check if modal form is open', () => {
+    const el = browser.$('//div[contains(@class, "sidepanel")]');
+    expect(el.isDisplayed()).true;
   });
 
-  it('should fill Group name field', () => {
-    const element = $(flashCardsSelectors.groupNameInput);
-    element.setValue(cardsPageData.groupName);
+  it('should check if modal form title is correct', () => {
+    const actualText = browser.$('//div[contains(@class, "sidepanel")]//*[@class="modal-title"]').getText();
+    const expectedText = 'Create Flash Group';
+    expect(actualText).eq(expectedText);
   });
 
-  it('should fill Group description field', () => {
-    const element = $(flashCardsSelectors.groupDescriptionInput);
-    element.setValue(cardsPageData.groupName);
+  it('should fill out input group name', () => {
+    const el = browser.$('//div[contains(@class, "sidepanel")]//input[@name="name"]');
+    el.setValue('My group name 555');
   });
 
-  it('should click Create button', () => {
-    const element = $(flashCardsSelectors.createButton);
-    element.click();
-    browser.pause(1000);
+  it('should fill out input group description', () => {
+    const el = browser.$('//div[contains(@class, "sidepanel")]//input[@name="description"]');
+    el.setValue('7777777');
   });
 
-  it('should check that error message does not pop-up', () => {
-    const element = $(flashCardsSelectors.messageInput);
-    expect(element).not.eql(cardsPageData.errorMessage);
-    browser.pause(1000);
+  it('should submit form', () => {
+    const el = browser.$('//div[contains(@class, "sidepanel")]//button[@type="submit"]');
+    el.click();
+    browser.pause(500);
   });
 
+  it('should first item in list be equal created group title', () => {
+    const actualTitle = browser.$('//div[@qa="flash-group-list "]//h4/a').getText();
+    const expectedTitle = 'My group name 555';
+    expect(actualTitle).eq(expectedTitle);
+  });
 
+  it('should first item in list be equal created group description', () => {
+    const actualDescription = browser.$('//div[@qa="flash-group-list "]//div[@qa="description"]').getText();
+    const expectedDescription = '7777777';
+    expect(actualDescription).eq(expectedDescription);
+  });
+
+  it('should verify created title group is clickable', () => {
+    const el = browser.$('//div[@qa="flash-group-list "]//h4/a');
+    el.click();
+    browser.pause(300);
+  });
+
+  it('should clicked group has correct title', () => {
+    const actual = browser.$('h1').getText();
+    const expected = 'My group name 555';
+    expect(actual).eq(expected);
+  });
+  
 });
